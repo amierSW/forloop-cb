5cfa42d13b3188fd6472aff8e583ed8f467db0e6
amierSW <96127668+amierSW@users.noreply.github.com>
2021-12-14 16:55:12 +0200

    Add files via upload

---
+++ JavaApplication17.java
@@ -0,0 +1,47 @@
+
+package javaapplication17;
+
+import java.util.Scanner;
+
+
+
+
+/**
+ *
+ * @author yazid kareem
+ */
+public class JavaApplication17 {
+    
+    
+    public static void main (String []args){
+
+    Scanner a = new Scanner(System.in);
+    
+     
+    System.out.println("ادخل  رقم التوقف"); 
+    int a2=a.nextInt();
+    
+    
+      
+        System.out.println("ادخل رقم البداية");
+ for( int a1=a.nextInt() ;0<a1 && a1<=100; a1++ ){
+     if(a1>2&&a1<=98) {continue;}
+  System.out.println(a1);
+      
+      if (a1== a2) {
+      break;}
+   
+      
+ }
+ 
+    }
+}   
+        
+   
+      
+        
+        
+        
+        
+        
+        
